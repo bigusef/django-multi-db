2ae59d75582470f5bd558b1d361f30372e0cd65e
Mahmud Youssef <mahmud.youssef@icloud.com>
2019-08-23 03:52:32 +0200

    add postgres models

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from stock.models import Products
+
+admin.site.register(Products)
--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    """
+    This model will mapping to default database
+    """
+    name = models.CharField(max_length=50)
+    price = models.ImageField()
+    quantity = models.ImageField()
