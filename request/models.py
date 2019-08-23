c8038da43b8e5a2491cd1f2627de29c48269edb1
Mahmud Youssef <mahmud.youssef@icloud.com>
2019-08-23 04:01:18 +0200

    add mongo models

--- request/models.py
+++ request/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Order(models.Model):
+    """
+    This model will mapping to mongo database
+    """
+    order_name = models.CharField(max_length=50)
+    invoice = models.FloatField()
